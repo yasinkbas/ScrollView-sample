eceaf8383b39fecd370f8fa0b6129c6a3a1d2125
yasinkbas <yasin.kbas12@gmail.com>
2019-10-20 00:12:20 +0300

    added page control

--- scrollView-sample/UserList.swift
+++ scrollView-sample/UserList.swift
@@ -12,9 +12,16 @@ class UserList: UIScrollView {
     
     var didSelectItem: ((_ index: Int)->())?
     
-    let buttonWidth: CGFloat = UIScreen.main.bounds.width
-    let buttonHeight: CGFloat = UIScreen.main.bounds.height
-    let padding: CGFloat = 30.0
+    
+    let pageWidth: CGFloat = UIScreen.main.bounds.width
+    let pageHeight: CGFloat = UIScreen.main.bounds.height
+    let padding: CGFloat = 10.0
+    
+    var page: Int {
+        get {
+            return Int(padding)
+        }
+    }
     
     required init(coder aDecoder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
@@ -34,33 +41,33 @@ class UserList: UIScrollView {
         
         alpha = 0.85
         
-        for i in 0 ..< 30 {
+        for i in 0 ..< page {
             
             //---------------------------- Page ----------------------------//
-            let item = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth - 40, height: buttonHeight - 120))
-            item.center = CGPoint(x: CGFloat(i) * buttonWidth + buttonWidth/2, y: buttonHeight/2)
-            item.tag = i
-            item.isUserInteractionEnabled = true
+            let page = UIView(frame: CGRect(x: 0, y: 0, width: pageWidth - 40, height: pageHeight - 120))
+            page.center = CGPoint(x: CGFloat(i) * pageWidth + pageWidth/2, y: pageHeight/2)
+            page.tag = i
+            page.isUserInteractionEnabled = true
             
-            item.layer.cornerRadius = 20
-            item.clipsToBounds = true
+            page.layer.cornerRadius = 20
+            page.clipsToBounds = true
             
             let gradient = CAGradientLayer()
-            gradient.frame = item.bounds
+            gradient.frame = page.bounds
             let custom1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
             let custom2 = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor
             gradient.colors = [custom1, custom2]
-            item.layer.insertSublayer(gradient, at: 0)
+            page.layer.insertSublayer(gradient, at: 0)
             
             //---------------------------- Profile Photo ----------------------------//
-            let profileWidth = item.frame.width / 3
-            let profileView = UIView(frame: CGRect(x: item.frame.width / 2 - profileWidth / 2, y: 50, width: profileWidth, height: profileWidth))
+            let profileWidth = page.frame.width / 3
+            let profileView = UIView(frame: CGRect(x: page.frame.width / 2 - profileWidth / 2, y: 50, width: profileWidth, height: profileWidth))
             profileView.layer.cornerRadius = profileView.bounds.height / 2
             profileView.backgroundColor = .white
-            item.addSubview(profileView)
+            page.addSubview(profileView)
             
             //---------------------------- Username ----------------------------//
-            let usernameWidth = item.frame.width
+            let usernameWidth = page.frame.width
             let username = UILabel(frame: CGRect(x: 0, y: 0, width: usernameWidth, height: 40))
             username.center = profileView.center
             username.frame.origin.y = profileView.frame.maxY + 8
@@ -68,10 +75,10 @@ class UserList: UIScrollView {
             username.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 24)
             username.textColor = .white
             username.textAlignment = .center
-            item.addSubview(username)
+            page.addSubview(username)
             
             //---------------------------- TextView ----------------------------//
-            let aboutTextView = UITextView(frame: CGRect(x: 0, y: 0, width: item.frame.width, height: item.frame.height / 2))
+            let aboutTextView = UITextView(frame: CGRect(x: 0, y: 0, width: page.frame.width, height: page.frame.height / 2))
             aboutTextView.backgroundColor = .clear
             aboutTextView.center = username.center
             aboutTextView.frame.origin.y = username.frame.maxY + 16
@@ -82,19 +89,20 @@ class UserList: UIScrollView {
             aboutTextView.textColor = .white
             aboutTextView.font = UIFont(name: "AvenirNextCondensed-Regular", size: 14)
             aboutTextView.isUserInteractionEnabled = false
-            item.addSubview(aboutTextView)
+            page.addSubview(aboutTextView)
             
-            addSubview(item)
+            addSubview(page)
+            
             //---------------------------- Gesture ----------------------------//
-            let tap = UITapGestureRecognizer(target: self, action: #selector(didTapImage(_:)))
-            item.addGestureRecognizer(tap)
+            let tap = UITapGestureRecognizer(target: self, action: #selector(didTapPage(_:)))
+            page.addGestureRecognizer(tap)
         }
         
-        contentSize = CGSize(width: padding * buttonWidth, height:  buttonWidth )
+        contentSize = CGSize(width: padding * pageWidth, height:  pageWidth )
     }
     
     //---------------------------- Gesture Func ----------------------------//
-    @objc func didTapImage(_ tap: UITapGestureRecognizer) {
+    @objc func didTapPage(_ tap: UITapGestureRecognizer) {
         didSelectItem?(tap.view!.tag)
     }
 }
--- scrollView-sample/ViewController.swift
+++ scrollView-sample/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
     var slider: UserList!
+    var pageControl: UIPageControl!
     
     var safeGuide: UILayoutGuide!
     
@@ -38,6 +39,7 @@ class ViewController: UIViewController {
         safeGuide = view.layoutMarginsGuide
         makeSlider()
         setUpMenuView()
+        makePageControl()
         
     }
     
@@ -59,7 +61,27 @@ class ViewController: UIViewController {
             print(index)
         }
         slider.isScrollEnabled = true
+        slider.delegate = self
         view.addSubview(slider)
+    }
+    
+    //---------------------------- Page control ----------------------------//
+    func makePageControl() {
+        let pageControlWidth = view.frame.width / 3
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: 0, width: pageControlWidth, height: 20))
+        pageControl.frame.origin = CGPoint(x: view.frame.midX - pageControlWidth / 2, y: view.frame.maxY - 30)
+        pageControl.numberOfPages = 10
+        pageControl.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        pageControl.pageIndicatorTintColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.989929378, green: 0.8843280673, blue: 0.1261277199, alpha: 1)
+        
+        view.addSubview(pageControl)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = round(slider.contentOffset.x / slider.frame.size.width)
+        pageControl.currentPage = Int(pageNumber)
+        print("worked")
     }
 }
 
